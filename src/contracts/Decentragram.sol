c660155ac53302d5189b68752e122c711331f7e2
GCAdigwe <gcadigwe@gmail.com>
2021-02-27 14:16:09 +0000

    wrote the upload image function in the smart contract and wrote tests for it

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,54 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
+  uint public imageCount = 0;
+
+
+
+  //store images
+  mapping(uint => Images) public images; 
+
+    struct Images {
+  	uint id;
+  	string hash;
+  	string description;
+  	uint tipAmount;
+  	address payable author;
+  }
+
+  event ImageCreated(
+  	uint id,
+  	string hash,
+  	string description,
+  	uint tipAmount,
+  	address payable author
+
+  	);
+
+
+  //create images
+
+  function uploadImage(string memory _imghash, string memory _imgdesc) public {
+  	require(bytes(_imghash).length > 0);
+  	require(bytes(_imgdesc).length > 0);
+  	require(msg.sender != address(0x0));
+  	imageCount++;
+
+  	//check if hash and description is > 0
+  
+
+  	//add images to mapping
+  	images[imageCount] = Images(imageCount,_imghash,_imgdesc,0,msg.sender);
+
+  	 //emitting event
+  	emit ImageCreated(imageCount,_imghash,_imgdesc,0,msg.sender);
+
+
+  }
+
+
+  //tip images
+
+
 }
